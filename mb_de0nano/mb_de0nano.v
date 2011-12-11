4150627597f5d146f34bd3963f20760e197fbc4b
iseroid <iseroid@gmail.com>
2011-12-11 21:39:13 +0900

    HW 構成整理 (SPI 周波数向上等)

--- mb_de0nano/mb_de0nano.v
+++ mb_de0nano/mb_de0nano.v
@@ -21,14 +21,15 @@ module mb_de0nano(
 	wire         w_max_sclk;
 	wire         w_max_mosi;
 	wire         w_max_miso;
-	wire [  7:0] w_pio_0;
+	wire [  7:0] w_pio_in;
+	wire [  7:0] w_pio_out;
 
 	assign clk = CLOCK_50;
 	assign w_rst_n = KEY[0];
 	assign w_max_miso = MAX_MISO;
-	assign w_pio_0[0] = MAX_INT;
-	assign w_pio_0[1] = MAX_GPX;
-	assign w_pio_0[7:2] = 6'd0;
+	assign w_pio_in[0] = MAX_INT;
+	assign w_pio_in[1] = MAX_GPX;
+	assign w_pio_in[7:2] = 6'd0;
 
 	always @( posedge clk or negedge w_rst_n ) begin
 		if( ! w_rst_n ) begin
@@ -39,16 +40,17 @@ module mb_de0nano(
 	end
 
 	mb_de0nano_sopc mb_de0nano_sopc(
-		.MISO_to_the_spi_0    ( w_max_miso ),
-		.MOSI_from_the_spi_0  ( w_max_mosi ),
-		.SCLK_from_the_spi_0  ( w_max_sclk ),
-		.SS_n_from_the_spi_0  ( w_max_ss ),
-		.clk_0                ( clk ),
-		.in_port_to_the_pio_0 ( w_pio_0 ),
-		.reset_n              ( rst_n )
+		.MISO_to_the_spi_usb       ( w_max_miso ),
+		.MOSI_from_the_spi_usb     ( w_max_mosi ),
+		.SCLK_from_the_spi_usb     ( w_max_sclk ),
+		.SS_n_from_the_spi_usb     ( w_max_ss ),
+		.clk_0                     ( clk ),
+		.in_port_to_the_pio_in     ( w_pio_in ),
+		.out_port_from_the_pio_out ( w_pio_out ),
+		.reset_n                   ( rst_n )
 	);
 
-	assign LED = 8'b0000_0000;
+	assign LED = w_pio_out;
 	assign MAX_RST  = rst_n;
 	assign MAX_SS   = w_max_ss;
 	assign MAX_SCLK = w_max_sclk;
